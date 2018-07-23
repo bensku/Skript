25acd70cb11380a6ae827b3653eecc6067852bd3
TheLimeGlass <seantgrover@gmail.com>
2018-07-23 05:26:51 -0600

    Add reverse list

--- src/main/java/ch/njol/skript/expressions/ExprShuffledList.java
+++ src/main/java/ch/njol/skript/expressions/ExprShuffledList.java
@@ -42,7 +42,7 @@ import ch.njol.util.Kleenean;
 
 @Name("Shuffled List")
 @Description("Shuffles given list randomly. This is done by replacing indices by random numbers in resulting list.")
-@Examples({"set {_list::*} to shuffled {_list::*}"})
+@Examples("set {_list::*} to shuffled {_list::*}")
 @Since("2.2-dev32")
 public class ExprShuffledList extends SimpleExpression<Object> {
 	
