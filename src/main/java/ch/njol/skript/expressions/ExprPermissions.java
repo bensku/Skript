b4084532159d8f3b4f577d12cf07b954a57dd720
TheLimeGlass <seantgrover@gmail.com>
2018-08-25 20:34:16 -0600

    Fix on permissions

--- src/main/java/ch/njol/skript/expressions/ExprPermissions.java
+++ src/main/java/ch/njol/skript/expressions/ExprPermissions.java
@@ -20,15 +20,11 @@
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.classes.Changer;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.PropertyExpression;
-import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
