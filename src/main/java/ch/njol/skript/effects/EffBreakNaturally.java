acba3f818eb4f17415196f96f8eace7be0811b21
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-01-26 04:39:29 -0800

    Updated docs for tool Bensku brought to my attention that 1.13.2 ignore enchants in the breakNaturally method. I removed the enchanted stuff from docs, and updating it to reflect what it truly does

--- src/main/java/ch/njol/skript/effects/EffBreakNaturally.java
+++ src/main/java/ch/njol/skript/effects/EffBreakNaturally.java
@@ -37,10 +37,10 @@ import ch.njol.util.Kleenean;
 @Name("Break Block")
 @Description({"Breaks the block and spawns items as if a player had mined it",
 		"\nYou can add a tool, which will spawn items based on how that tool would break the block ",
-		"(ie: Using a diamond pickaxe of fortune 3 would drop more coal items when breaking a coal ore block)"})
+		"(ie: When using a hand to break stone, it drops nothing, whereas with a pickaxe it drops cobblestone)"})
 @Examples({"on right click:", "\tbreak clicked block naturally",
 		"loop blocks in radius 10 around player:", "\tbreak loop-block using player's tool",
-		"loop blocks in radius 10 around player:", "\tbreak loop-block naturally using diamond pickaxe of fortune 3"})
+		"loop blocks in radius 10 around player:", "\tbreak loop-block naturally using diamond pickaxe"})
 @Since("INSERT VERSION")
 public class EffBreakNaturally extends Effect {
 	
