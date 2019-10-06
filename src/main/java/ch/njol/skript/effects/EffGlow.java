27935a598ef82fd05206c97d68d967d6fcdf304b
Wealthyturtle <limbennett@yahoo.com.sg>
2019-10-06 13:41:50 +0800

    Glow Effect

---
+++ src/main/java/ch/njol/skript/effects/EffGlow.java
@@ -0,0 +1,73 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright 2011-2017 Peter Güttinger and contributors
+ */
+package ch.njol.skript.effects;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+@Name("Glow")
+@Description("Makes an entity glow / not glow.")
+@Examples("make target entity glow")
+@Since("INSERT VERSION")
+public class EffGlow extends Effect {
+	
+	static {
+		Skript.registerEffect(EffGlow.class,
+			"make %entities% glow",
+			"make %entities% not glow");
+	}
+	
+	@SuppressWarnings("null")
+	private Expression<Entity> expr;
+	private boolean glow;
+	
+	@SuppressWarnings({"unchecked", "null"})
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
+		expr = (Expression<Entity>) exprs[0];
+		glow = (matchedPattern == 0);
+		return true;
+	}
+	
+	@Override
+	protected void execute(final Event e) {
+		for (Entity entity : expr.getArray(e)) {
+			entity.setGlowing(glow);
+		}
+	}
+	
+	@Override
+	public String toString(final @Nullable Event e, final boolean debug) {
+		return "make" + expr.toString(e, debug) + (glow ? "glow" : "not glow");
+	}
+	
+}
