691705e27f145cbe73ce88eb9237a03b1684bc09
Wealthyturtle <limbennett@yahoo.com.sg>
2019-10-22 10:39:09 +0800

    Update CondIsSolid.java

--- src/main/java/ch/njol/skript/conditions/CondIsSolid.java
+++ src/main/java/ch/njol/skript/conditions/CondIsSolid.java
@@ -40,8 +40,7 @@ public class CondIsSolid extends PropertyCondition<ItemType> {
 	
 	@Override
 	public boolean check(ItemType i) {
-		ItemStack stack = i.getRandom();
-		return stack == null ? false : stack.getType().isSolid();
+		return i.getMaterial().isSolid();
 	}
 	
 	@Override
