2b0003d4638101e23c3eb33b75237d3b7830e759
Wealthyturtle <limbennett@yahoo.com.sg>
2019-10-28 15:16:12 +0800

    Update SkriptConfig.java

--- src/main/java/ch/njol/skript/SkriptConfig.java
+++ src/main/java/ch/njol/skript/SkriptConfig.java
@@ -344,6 +344,7 @@ public abstract class SkriptConfig {
 	/**
 	 * Returns the previous config file version, before Skript automatically updates the version number
 	 */
+	@Nullable
 	public static Version getPreviousConfigVersion() {
 		return previousConfigVersion;
 	}
