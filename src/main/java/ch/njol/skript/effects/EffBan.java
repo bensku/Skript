63c1099a6fb3abf03f745b2e91b056baabcad9d6
Jayderp <49515406+Jayderp@users.noreply.github.com>
2019-12-15 04:33:58 +1300

    FIXED LIKE HOW I FIXED MY LAPTOP LIKE A YEAR AGO

--- src/main/java/ch/njol/skript/effects/EffBan.java
+++ src/main/java/ch/njol/skript/effects/EffBan.java
@@ -88,7 +88,7 @@ public class EffBan extends Effect {
 	@Override
 	protected void execute(final Event e) {
 		final String reason = this.reason != null ? this.reason.getSingle(e) : null; // don't check for null, just ignore an invalid reason
-		final Date expires = new Date(System.currentTimeMillis() + this.duration.getSingle(e).getMilliSeconds());
+		final Date expires = this.duration != null ? new Date(System.currentTimeMillis() + this.duration.getSingle(e).getMilliSeconds()) : null;
 		final String source = "Skript ban effect";
 		for (final Object o : players.getArray(e)) {
 			if (o instanceof Player) {
