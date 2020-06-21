0ef9d70f97dae0b74420b7d103984a5e3763cc43
TPGamesNL <pet.teun.03@gmail.com>
2020-06-21 20:22:34 +0200

    Create PostStructureCreateEvent.java

---
+++ src/main/java/ch/njol/skript/events/bukkit/PostStructureCreateEvent.java
@@ -0,0 +1,52 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright 2011-2017 Peter Güttinger and contributors
+ */
+package ch.njol.skript.events.bukkit;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import ch.njol.skript.config.Config;
+import ch.njol.util.Validate;
+
+public class PostStructureCreateEvent extends Event {
+
+	private Config script;
+
+	public PostStructureCreateEvent(Config script) {
+		Validate.notNull(script);
+		this.script = script;
+	}
+
+	private static HandlerList handlers = new HandlerList();
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+	public Config getScript() {
+		return script;
+	}
+
+}
