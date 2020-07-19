71d11aab90e3b3febbf6d8cb5341a601239bdcd3
APickledWalrus <apickledwalrus@gmail.com>
2020-07-19 15:52:58 -0400

    Update changer, patterns, and formatting

--- src/main/java/ch/njol/skript/expressions/ExprExplosionBlockYield.java
+++ src/main/java/ch/njol/skript/expressions/ExprExplosionBlockYield.java
@@ -39,25 +39,27 @@ import ch.njol.skript.log.ErrorQuality;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
 
-@Name("Explosion Event Block Yield")
-@Description({"The percentage (in decimal form) of exploded blocks dropped in an explosion event.", 
-				"When changing the yield, a value greater than 1 will be converted to a decimal.",
-				"For example, attempting to set the yield to 50 will actually set it to 0.50 (50%)."})
+@Name("Explosion Block Yield")
+@Description({"The percentage of exploded blocks dropped in an explosion event.",
+				"When changing the yield, a value greater than 1 will function the same as using 1.",
+				"Attempting to change the yield to a value less than 0 will have no effect."})
+@Examples({"on explode:",
+			"set the explosion's block yield to 10%"})
 @Events("explosion")
-@Examples("set the explosion's block yield to 10%")
 @Since("INSERT VERSION")
-public class ExprExplosionEventBlockYield extends SimpleExpression<Number> {
-	
+public class ExprExplosionBlockYield extends SimpleExpression<Number> {
+
 	static {
-		Skript.registerExpression(ExprExplosionEventBlockYield.class, Number.class, ExpressionType.PROPERTY, 
+		Skript.registerExpression(ExprExplosionBlockYield.class, Number.class, ExpressionType.PROPERTY,
 				"[the] [explosion['s]] block (yield|amount)",
-				"[the] block (yield|amount) of [the] explosion");
+				"[the] percentage of blocks dropped"
+		);
 	}
-	
+
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		if (!ScriptLoader.isCurrentEvent(EntityExplodeEvent.class)) {
-			Skript.error("The explosion yield is only usable in an explosion event", ErrorQuality.SEMANTIC_ERROR);
+			Skript.error("The 'explosion block yield' is only usable in an explosion event", ErrorQuality.SEMANTIC_ERROR);
 			return false;
 		}
 		return true;
@@ -66,7 +68,7 @@ public class ExprExplosionEventBlockYield extends SimpleExpression<Number> {
 	@Override
 	@Nullable
 	protected Number[] get(Event e) {
-		return CollectionUtils.array(((EntityExplodeEvent) e).getYield());
+		return new Number[]{((EntityExplodeEvent) e).getYield()};
 	}
 
 	@Override
@@ -80,7 +82,10 @@ public class ExprExplosionEventBlockYield extends SimpleExpression<Number> {
 	@Override
 	public void change(final Event event, final @Nullable Object[] delta, final ChangeMode mode) {
 		float n = delta == null ? 0 : ((Number) delta[0]).floatValue();
-		if (n > 1) n = n / 100;
+		if (n < 0) // Yield can't be negative
+			return;
+		if (n > 1) // Treat 'set the explosion's block yield to 500' as setting it to the maximum value (i.e. 1)
+			n = 1;
 		EntityExplodeEvent e = (EntityExplodeEvent) event;
 		switch (mode) {
 			case SET:
@@ -88,14 +93,18 @@ public class ExprExplosionEventBlockYield extends SimpleExpression<Number> {
 				break;
 			case ADD:
 				float add = e.getYield() + n;
-				if (add < 0) add = 0;
-				if (add > 1) add = 1;
+				if (add < 0)
+					return;
+				if (add > 1)
+					add = 1;
 				e.setYield(add);
 				break;
 			case REMOVE:
 				float subtract = e.getYield() - n;
-				if (subtract < 0) subtract = 0;
-				if (subtract > 1) subtract = 1;
+				if (subtract < 0)
+					return;
+				if (subtract > 1)
+					subtract = 1;
 				e.setYield(subtract);
 				break;
 			case DELETE:
@@ -108,18 +117,18 @@ public class ExprExplosionEventBlockYield extends SimpleExpression<Number> {
 	}
 
 	@Override
-	public Class<? extends Number> getReturnType() {
-		return Number.class;
-	}
-
-	@Override
 	public boolean isSingle() {
 		return true;
 	}
 
 	@Override
-	public String toString(@Nullable Event e, boolean debug) {
-		return "explosion event block yield";
+	public Class<? extends Number> getReturnType() {
+		return Number.class;
 	}
-	
+
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return "the explosion's block yield";
+	}
+
 }
