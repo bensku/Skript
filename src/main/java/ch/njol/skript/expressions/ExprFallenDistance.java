885151687805914f4a750aa8d798ce97bbd1ee02
DeltaRays <realplaxyz@gmail.com>
2020-07-20 12:03:23 +0200

    Update ExprFallenDistance.java

--- src/main/java/ch/njol/skript/expressions/ExprFallenDistance.java
+++ src/main/java/ch/njol/skript/expressions/ExprFallenDistance.java
@@ -31,7 +31,7 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.util.coll.CollectionUtils;
 
-@Name("Fall distance")
+@Name("Fall Distance")
 @Description({"The distance an entity has fallen for"})
 @Examples({"set all entities' fall distance to 10",
 	"on damage:",
