1331ef806f5cc35a11a8d32cd6e3de06a536e6ab
DeltaRays <realplaxyz@gmail.com>
2020-07-26 13:39:38 +0200

    Created EffStopServer

---
+++ src/main/java/ch/njol/skript/effects/EffStopServer.java
@@ -0,0 +1,53 @@
+/**
+ * This file is part of Skript.
+ *
+ * Skript is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Skript is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright 2011-2017 Peter Güttinger and contributors
+ */
+package ch.njol.skript.effects;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+public class EffStopServer extends Effect {
+	
+	static {
+		Skript.registerEffect(EffStopServer.class, "(stop|shutdown) [the] server");
+	}
+	
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		return true;
+	}
+	
+	@Override
+	protected void execute(Event e) {
+		Bukkit.getServer().shutdown();
+	}
+	
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return "stop the server";
+	}
+	
+}
