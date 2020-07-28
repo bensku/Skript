8cf533cd2269d4f6935c1958e29cf0c702e141f4
DeltaRays <realplaxyz@gmail.com>
2020-07-28 11:58:28 +0200

    Added a . to the end of the description

--- src/main/java/ch/njol/skript/expressions/ExprSneakingState.java
+++ src/main/java/ch/njol/skript/expressions/ExprSneakingState.java
@@ -33,7 +33,7 @@ import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.util.coll.CollectionUtils;
 
 @Name("Sneaking State")
-@Description("Whether the player(s) are sneaking. Note that it doesn't force them to sneak, it just changes how other players see the players")
+@Description("Whether the player(s) are sneaking. Note that it doesn't force them to sneak, it just changes how other players see the players.")
 @Examples({"set sneak mode of player to true", "send \"%sneaking state of all players%\""})
 @Since("INSERT VERSION")
 public class ExprSneakingState extends SimplePropertyExpression<Player, Boolean> {
