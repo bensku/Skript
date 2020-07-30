3ff1990fafe31a22a461111bc63856055a174810
DeltaRays <realplaxyz@gmail.com>
2020-07-30 23:26:58 +0200

    Fixed everything

--- src/main/java/ch/njol/skript/conditions/CondIsAdult.java
+++ src/main/java/ch/njol/skript/conditions/CondIsAdult.java
@@ -22,58 +22,31 @@ package ch.njol.skript.conditions;
 import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Zombie;
-import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
 
-import ch.njol.skript.Skript;
+import ch.njol.skript.conditions.base.PropertyCondition;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.Condition;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.util.Kleenean;
 
 @Name("Is Adult")
 @Description("Whether or not an entity is an adult.")
 @Examples("player's target is an adult")
 @Since("INSERT VERSION")
-public class CondIsAdult extends Condition {
-	
+public class CondIsAdult extends PropertyCondition<Entity> {
 	static {
-		Skript.registerCondition(CondIsAdult.class, "%entity% (1¦is|2¦is(n't| not)) [a[n]] adult");
-	}
-	
-	@SuppressWarnings("null")
-	private Expression<Entity> entities;
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
-		entities = (Expression<Entity>) exprs[0];
-		setNegated(parser.mark == 2);
-		return true;
+		register(CondIsAdult.class, PropertyType.BE, "adult", "entity");
 	}
 	
 	@Override
-	public boolean check(Event e) {
-		if (entities.getAnd())
-			return entities.stream(e).allMatch(entity -> {
-				if (entity instanceof Ageable) return ((Ageable) entity).isAdult();
-				else if (entity instanceof Zombie) return !((Zombie) entity).isBaby();
-				else return false;
-			}) != isNegated();
-		else
-			return entities.stream(e).anyMatch(entity -> {
-				if (entity instanceof Ageable) return ((Ageable) entity).isAdult();
-				else if (entity instanceof Zombie) return !((Zombie) entity).isBaby();
-				else return false;
-			}) != isNegated();
+	public boolean check(Entity entity) {
+		if (entity instanceof Ageable) return ((Ageable) entity).isAdult();
+		else if (entity instanceof Zombie) return !((Zombie) entity).isBaby();
+		else return false;
 	}
 	
 	@Override
-	public String toString(@Nullable Event e, boolean debug) {
-		return entities.toString(e, debug) + (isNegated() ? " isn't " : " is ") + "an adult";
+	protected String getPropertyName() {
+		return "is adult";
 	}
 }
