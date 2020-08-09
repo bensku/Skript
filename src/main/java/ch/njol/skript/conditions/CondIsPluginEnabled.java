513144472433916b2ae6b8de55685cbff23f3fa6
Shane Bee <shanebolenback@me.com>
2020-08-09 13:22:48 -0700

    CondIsPluginEnabled - added option for "is disabled" and updated docs

--- src/main/java/ch/njol/skript/conditions/CondIsPluginEnabled.java
+++ src/main/java/ch/njol/skript/conditions/CondIsPluginEnabled.java
@@ -35,24 +35,29 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 
 @Name("Is Plugin Enabled")
-@Description("Check if a plugin is enabled on the server.")
-@Examples({"if plugin \"Vault\" is enabled:", "if plugin \"WorldGuard\" is not enabled:", "if plugins \"Essentials\" and \"Vault\" are enabled:"})
+@Description({"Check if a plugin is enabled/disabled on the server.",
+		"Plugin names can be found in the plugin's 'plugin.yml' file or by using the '/plugins' command, they are NOT the name of the plugin's jar file.",
+		"When checking if a plugin is not enabled, this will return true if the plugin is either disabled or not on the server. ",
+		"When checking if a plugin is disabled, this will return true if the plugin is on the server and is disabled."})
+@Examples({"if plugin \"Vault\" is enabled:", "if plugin \"WorldGuard\" is not enabled:",
+		"if plugins \"Essentials\" and \"Vault\" are enabled:", "if plugin \"MyBrokenPlugin\" is disabled:"})
 @Since("INSERT VERSION")
 public class CondIsPluginEnabled extends Condition {
 	
 	static {
 		Skript.registerCondition(CondIsPluginEnabled.class, "plugin[s] %strings% (is|are) enabled",
-				"plugin[s] %strings% (is|are)(n't| not) enabled");
+				"plugin[s] %strings% (is|are)(n't| not) enabled", "plugin[s] %strings% (is|are) disabled");
 	}
 	
 	@SuppressWarnings("null")
 	private Expression<String> plugins;
+	private int pattern;
 	
 	@SuppressWarnings({"unchecked", "null"})
 	@Override
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
 		plugins = (Expression<String>) exprs[0];
-		setNegated(matchedPattern == 1);
+		pattern = matchedPattern;
 		return true;
 	}
 	
@@ -60,16 +65,23 @@ public class CondIsPluginEnabled extends Condition {
 	public boolean check(Event e) {
 		return plugins.check(e, plugin -> {
 			Plugin p = Bukkit.getPluginManager().getPlugin(plugin);
-			return p != null && p.isEnabled();
-		}, isNegated());
+			switch (pattern) {
+				case 1:
+					return p == null || !p.isEnabled();
+				case 2:
+					return p != null && !p.isEnabled();
+				default:
+					return p != null && p.isEnabled();
+			}
+		});
 	}
 	
 	@Override
 	public String toString(@Nullable Event e, boolean debug) {
 		String plugin = plugins.isSingle() ? "plugin " : "plugins ";
 		String plural = plugins.isSingle() ? " is" : " are";
-		String neg = isNegated() ? " not" : "";
-		return plugin + plugins.toString(e, debug) + plural + neg + "  enabled.";
+		String pattern = this.pattern == 0 ? " enabled" : this.pattern == 1 ? " not enabled" : " disabled";
+		return plugin + plugins.toString(e, debug) + plural + pattern;
 	}
 	
 }
