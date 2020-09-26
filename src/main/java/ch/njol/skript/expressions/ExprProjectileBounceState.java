b1bfbb9ea731b1452b0540859cbd1ed64a60b96d
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2020-09-26 21:01:38 +0200

    Made ExprProjectileBounceState do nothing if delta is null

--- src/main/java/ch/njol/skript/expressions/ExprProjectileBounceState.java
+++ src/main/java/ch/njol/skript/expressions/ExprProjectileBounceState.java
@@ -1,18 +1,18 @@
 /**
- *   This file is part of Skript.
+ * This file is part of Skript.
  *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Skript is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Skript is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
  * Copyright 2011-2017 Peter Güttinger and contributors
@@ -56,7 +56,8 @@ public class ExprProjectileBounceState extends SimplePropertyExpression<Projecti
 	
 	@Override
 	public void change(Event e, @Nullable Object[] delta, ChangeMode mode) {
-		boolean state = delta != null ? (Boolean) delta[0] : false;
+		if (delta == null) return;
+		boolean state = (Boolean) delta[0];
 		for (Projectile entity : getExpr().getArray(e))
 			entity.setBounce(state);
 	}
