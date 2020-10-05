9c6d5a778545a42e75829f81847b7761722ef0ca
youngmasterseth <44213317+youngmasterseth@users.noreply.github.com>
2020-10-05 11:07:12 -0700

    Update CondIsVectorNormalized.java

--- src/main/java/ch/njol/skript/conditions/CondIsVectorNormalized.java
+++ src/main/java/ch/njol/skript/conditions/CondIsVectorNormalized.java
@@ -1,22 +1,21 @@
 /**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * Copyright 2011-2017 Peter Güttinger and contributors
- */
+*  This file is part of Skript.
+*
+* Skript is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Skript is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright Peter Güttinger, SkriptLang team and contributors
+*/
 package ch.njol.skript.conditions;
 	
 	import org.bukkit.util.Vector;
@@ -33,11 +32,13 @@ package ch.njol.skript.conditions;
 @Description("Checks whether a vector is normalized i.e. length of 1")
 @Examples("vector of player's location is normalized")
 @Since("INSERT VERSION")
-@RequiredPlugins("1.13.2 or newer")
+@RequiredPlugins("Minecraft 1.13.2+")
 public class CondIsVectorNormalized extends PropertyCondition<Vector> {
 	
 	static {
-		if (Skript.methodExists(Vector.class, "isNormalized")) {register(CondIsVectorNormalized.class, "normalized", "vectors");}
+		if (Skript.methodExists(Vector.class, "isNormalized")) {
+			register(CondIsVectorNormalized.class, "normalized", "vectors");
+		}
 	}
 	
 	@Override
