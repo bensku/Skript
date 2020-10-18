bensku/Skript

commit 048ce296c2c3427bbbb5f9d128d150b802de0df3

Make AsyncEffect not leak memory

src/main/java/ch/njol/skript/util/AsyncEffect.java
@@ -25,6 +25,7 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Trigger;
 import ch.njol.skript.lang.TriggerItem;
 import ch.njol.skript.timings.SkriptTimings;
+import ch.njol.skript.variables.Variables;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
@@ -38,6 +39,7 @@ import java.util.concurrent.Executors;
  * in main server thread, as if there had been a delay before.
  * <p>
  * Majority of Skript and Minecraft APIs are not thread-safe, so be careful.
+ * Also, local variables are not available while executing asynchronous code.
  */
 public abstract class AsyncEffect extends Effect {
 	
@@ -46,16 +48,24 @@ public abstract class AsyncEffect extends Effect {
 	protected TriggerItem walk(Event e) {
 		debug(e, true);
 		TriggerItem next = getNext();
-		Delay.addDelayedEvent(e);
-		Bukkit.getScheduler().runTaskAsynchronously(Skript.getInstance(), new Runnable() {
-	        	@SuppressWarnings("synthetic-access")
-			@Override
-	            	public void run() {
-				execute(e); // Execute this effect
-	                	if (next != null) {
+		
+		if (next != null) {
+			Delay.addDelayedEvent(e); // Mark this event as delayed
+			Object localVars = Variables.removeLocals(e); // Back up local variables
+			
+			Bukkit.getScheduler().runTaskAsynchronously(Skript.getInstance(), new Runnable() {
+				@SuppressWarnings("synthetic-access")
+				@Override
+				public void run() {
+					execute(e); // Execute this effect
+					
 					Bukkit.getScheduler().runTask(Skript.getInstance(), new Runnable() {
 						@Override
 						public void run() { // Walk to next item synchronously
+							// Re-set local variables
+							if (localVars != null)
+								Variables.setLocalVariables(e, localVars);
+							
 							Object timing = null;
 							if (SkriptTimings.enabled()) { // getTrigger call is not free, do it only if we must
 								Trigger trigger = getTrigger();
@@ -63,15 +73,16 @@ public abstract class AsyncEffect extends Effect {
 									timing = SkriptTimings.start(trigger.getDebugLabel());
 								}
 							}
-
+							
 							TriggerItem.walk(next, e);
-
+							Variables.removeLocals(e); // Clean up local vars, we may be exiting now
+							
 							SkriptTimings.stop(timing); // Stop timing if it was even started
 						}
 					});	
 				}
-	            	}
-	        });
-        return null;
-    }
+			});
+		}
+		return null;
+	}
 }

src/main/java/ch/njol/skript/effects/Delay.java
@@ -107,7 +107,7 @@ public class Delay extends Effect {
 					}
 					
 					TriggerItem.walk(next, e);
-					Variables.removeLocals(e);
+					Variables.removeLocals(e); // Clean up local vars, we may be exiting now
 					
 					SkriptTimings.stop(timing); // Stop timing if it was even started
 				}

commit a31c2d55acdddeca1c20f40ae4e64180c893c78b

Fix AsyncEffect not being executed when last in trigger
This fixes a regression from 048ce296c2c3427bbbb5f9d128d150b802de0df3 which
moved the next TriggerItem check above the actual execution of the AsyncEffect.
This caused a no-op when an AsyncEffect was the last in a trigger

src/main/java/ch/njol/skript/util/AsyncEffect.java
@@ -48,40 +48,39 @@ public abstract class AsyncEffect extends Effect {
 		debug(e, true);
 		TriggerItem next = getNext();
 		
-		if (next != null) {
-			Delay.addDelayedEvent(e); // Mark this event as delayed
-			Object localVars = Variables.removeLocals(e); // Back up local variables
-			
-			Bukkit.getScheduler().runTaskAsynchronously(Skript.getInstance(), new Runnable() {
-				@SuppressWarnings("synthetic-access")
-				@Override
-				public void run() {
-					execute(e); // Execute this effect
-					
-					Bukkit.getScheduler().runTask(Skript.getInstance(), new Runnable() {
-						@Override
-						public void run() { // Walk to next item synchronously
-							// Re-set local variables
-							if (localVars != null)
-								Variables.setLocalVariables(e, localVars);
-							
-							Object timing = null;
+		Delay.addDelayedEvent(e); // Mark this event as delayed
+		Object localVars = Variables.removeLocals(e); // Back up local variables
+		
+		Bukkit.getScheduler().runTaskAsynchronously(Skript.getInstance(), new Runnable() {
+			@SuppressWarnings("synthetic-access")
+			@Override
+			public void run() {
+				execute(e); // Execute this effect
+				
+				Bukkit.getScheduler().runTask(Skript.getInstance(), new Runnable() {
+					@Override
+					public void run() { // Walk to next item synchronously
+						// Re-set local variables
+						if (localVars != null)
+							Variables.setLocalVariables(e, localVars);
+						
+						Object timing = null;
+						if (next != null) {
 							if (SkriptTimings.enabled()) { // getTrigger call is not free, do it only if we must
 								Trigger trigger = getTrigger();
 								if (trigger != null) {
 									timing = SkriptTimings.start(trigger.getDebugLabel());
 								}
 							}
-							
 							TriggerItem.walk(next, e);
-							Variables.removeLocals(e); // Clean up local vars, we may be exiting now
-							
-							SkriptTimings.stop(timing); // Stop timing if it was even started
 						}
-					});	
-				}
-			});
-		}
+						Variables.removeLocals(e); // Clean up local vars, we may be exiting now
+						
+						SkriptTimings.stop(timing); // Stop timing if it was even started
+					}
+				});
+			}
+		});
 		return null;
 	}
 }