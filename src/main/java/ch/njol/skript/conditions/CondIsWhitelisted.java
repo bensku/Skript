b63a3b78c987da3af693c20f0b5cf366fd96cb2c
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2020-11-05 21:11:02 +0100

    Reverted license header changes and added support to multiple players

--- src/main/java/ch/njol/skript/conditions/CondIsWhitelisted.java
+++ src/main/java/ch/njol/skript/conditions/CondIsWhitelisted.java
@@ -1,19 +1,18 @@
 /**
- * This file is part of Skript.
+ *   This file is part of Skript.
  *
- * Skript is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * Skript is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright Peter Güttinger, SkriptLang team and contributors
  */
@@ -41,29 +40,45 @@ import ch.njol.util.Kleenean;
 public class CondIsWhitelisted extends Condition {
 	
 	static {
-		Skript.registerCondition(CondIsWhitelisted.class, "[the] server (is|1¦is(n't| not)) white[ ]listed", "%player% (is|1¦is(n't| not)) white[ ]listed");
+		Skript.registerCondition(CondIsWhitelisted.class,
+			"[the] server (is|1¦is(n't| not)) white[ ]listed",
+			"%players% (is|are)(|1¦(n't| not)) white[ ]listed");
 	}
 	
 	@Nullable
 	private Expression<Player> player;
 	
+	private boolean isServer;
+	
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		setNegated(parseResult.mark != 1);
-		if (matchedPattern == 1) {
+		setNegated(parseResult.mark == 1);
+		isServer = matchedPattern == 0;
+		if (matchedPattern == 1)
 			player = (Expression<Player>) exprs[0];
-		}
 		return true;
 	}
 	
 	@Override
 	@SuppressWarnings("null")
 	public boolean check(Event e) {
-		if (player != null) {
-			if (player.getSingle(e) == null) return false;
-			return player.getSingle(e).isWhitelisted() == isNegated();
+		if (isServer)
+			return Bukkit.hasWhitelist() == isNegated();
+		Player[] players = player.getAll(e);
+		if (player.getAnd() && isNegated()) {
+			for (Player player : players)
+				if (player.isWhitelisted())
+					return false;
+		} else if(player.getAnd()){
+			for (Player player : players)
+				if (!player.isWhitelisted())
+					return false;
+		} else {
+			for (Player player: players)
+				if(player.isWhitelisted())
+					return !isNegated();
 		}
-		return Bukkit.hasWhitelist() == isNegated();
+		return !isNegated();
 	}
 	
 	@Override
