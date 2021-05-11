da04a2c00f23e6bc8769c844522a5cd8f433069b
TPGamesNL <pet.teun.03@gmail.com>
2021-05-11 11:43:25 +0200

    Update Language.java

--- src/main/java/ch/njol/skript/localization/Language.java
+++ src/main/java/ch/njol/skript/localization/Language.java
@@ -104,13 +104,13 @@ public class Language {
 	}
 	
 	@Nullable
-	private static String get_i(final String key) {
+	private static String get_i(String key) {
 		if (useLocal && localized != null) {
-			final String s = localized.get(key);
+			String s = localized.get(key);
 			if (s != null)
 				return s;
 		}
-		final String s = english.get(key);
+		String s = english.get(key);
 		if (s == null && Skript.testing())
 			missingEntryError(key);
 		return s;
@@ -123,8 +123,8 @@ public class Language {
 	 * @param key The message's key (case-insensitive)
 	 * @return The requested message if it exists or the key otherwise
 	 */
-	public static String get(final String key) {
-		final String s = get_i("" + key.toLowerCase(Locale.ENGLISH));
+	public static String get(String key) {
+		String s = get_i("" + key.toLowerCase(Locale.ENGLISH));
 		return s == null ? "" + key.toLowerCase(Locale.ENGLISH) : s;
 	}
 	
@@ -135,11 +135,11 @@ public class Language {
 	 * @return The requested message or null if it doesn't exist
 	 */
 	@Nullable
-	public static String get_(final String key) {
+	public static String get_(String key) {
 		return get_i("" + key.toLowerCase(Locale.ENGLISH));
 	}
 	
-	public static void missingEntryError(final String key) {
+	public static void missingEntryError(String key) {
 		Skript.error("Missing entry '" + key.toLowerCase(Locale.ENGLISH) + "' in the default english language file");
 	}
 	
@@ -150,14 +150,14 @@ public class Language {
 	 * @param args The arguments to pass to {@link String#format(String, Object...)}
 	 * @return The formatted string
 	 */
-	public static String format(String key, final Object... args) {
+	public static String format(String key, Object... args) {
 		key = "" + key.toLowerCase(Locale.ENGLISH);
-		final String value = get_i(key);
+		String value = get_i(key);
 		if (value == null)
 			return key;
 		try {
 			return "" + String.format(value, args);
-		} catch (final Exception e) {
+		} catch (Exception e) {
 			Skript.error("Invalid format string at '" + key + "' in the " + getName() + " language file: " + value);
 			return key;
 		}
@@ -169,8 +169,8 @@ public class Language {
 	 * @param key
 	 * @return The message surrounded by spaces, a space if the entry is empty, or " "+key+" " if the entry is missing.
 	 */
-	public static String getSpaced(final String key) {
-		final String s = get(key);
+	public static String getSpaced(String key) {
+		String s = get(key);
 		if (s.isEmpty())
 			return " ";
 		return " " + s + " ";
@@ -185,11 +185,11 @@ public class Language {
 	 * @param key
 	 * @return a non-null String array with at least one element
 	 */
-	public static String[] getList(final String key) {
-		final String s = get_i("" + key.toLowerCase(Locale.ENGLISH));
+	public static String[] getList(String key) {
+		String s = get_i("" + key.toLowerCase(Locale.ENGLISH));
 		if (s == null)
 			return new String[] {key.toLowerCase(Locale.ENGLISH)};
-		final String[] r = listSplitPattern.split(s);
+		String[] r = listSplitPattern.split(s);
 		assert r != null;
 		return r;
 	}
@@ -198,33 +198,33 @@ public class Language {
 	 * @param key
 	 * @return Whether the given key exists in the <b>english</b> language file.
 	 */
-	public static boolean keyExists(final String key) {
+	public static boolean keyExists(String key) {
 		return english.containsKey(key.toLowerCase(Locale.ENGLISH));
 	}
 	
-	public static void loadDefault(final SkriptAddon addon) {
+	public static void loadDefault(SkriptAddon addon) {
 		if (addon.getLanguageFileDirectory() == null)
 			return;
-		final InputStream din = addon.plugin.getResource(addon.getLanguageFileDirectory() + "/english.lang");
+		InputStream din = addon.plugin.getResource(addon.getLanguageFileDirectory() + "/english.lang");
 		if (din == null)
 			throw new IllegalStateException(addon + " is missing the required english.lang file!");
 		HashMap<String, String> en;
 		try {
 			en = new Config(din, "english.lang", false, false, ":").toMap(".");
-		} catch (final Exception e) {
+		} catch (Exception e) {
 			throw Skript.exception(e, "Could not load " + addon + "'s default language file!");
 		} finally {
 			try {
 				din.close();
-			} catch (final IOException e) {}
+			} catch (IOException e) {}
 		}
-		final String v = en.get("version");
+		String v = en.get("version");
 		if (v == null)
 			Skript.warning("Missing version in english.lang");
 		langVersion.put(addon.plugin, v == null ? Skript.getVersion() : new Version(v));
 		en.remove("version");
 		english.putAll(en);
-		for (final LanguageChangeListener l : listeners)
+		for (LanguageChangeListener l : listeners)
 			l.onLanguageChange();
 	}
 	
@@ -234,8 +234,7 @@ public class Language {
 			return true;
 		localized = new HashMap<>();
 		boolean exists = load(Skript.getAddonInstance(), name);
-		for (final SkriptAddon addon : Skript.getAddons()) {
-			assert addon != null;
+		for (SkriptAddon addon : Skript.getAddons()) {
 			exists |= load(addon, name);
 		}
 		if (!exists) {
@@ -244,23 +243,22 @@ public class Language {
 			return false;
 		}
 		Language.name = name;
-		validateLocalized();
 		if (useLocal) {
-			for (final LanguageChangeListener l : listeners)
+			for (LanguageChangeListener l : listeners)
 				l.onLanguageChange();
 		}
 		return true;
 	}
 	
-	private static boolean load(final SkriptAddon addon, final String name) {
+	private static boolean load(SkriptAddon addon, String name) {
 		if (addon.getLanguageFileDirectory() == null)
 			return false;
-		final HashMap<String, String> l = load(addon.plugin.getResource(addon.getLanguageFileDirectory() + "/" + name + ".lang"), name);
-		final File f = new File(addon.plugin.getDataFolder(), addon.getLanguageFileDirectory() + File.separator + name + ".lang");
+		HashMap<String, String> l = load(addon.plugin.getResource(addon.getLanguageFileDirectory() + "/" + name + ".lang"), name);
+		File f = new File(addon.plugin.getDataFolder(), addon.getLanguageFileDirectory() + File.separator + name + ".lang");
 		try {
 			if (f.exists())
 				l.putAll(load(new FileInputStream(f), name));
-		} catch (final FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			assert false;
 		}
 		if (l.isEmpty())
@@ -269,17 +267,17 @@ public class Language {
 			Skript.error(addon + "'s language file " + name + ".lang does not provide a version number!");
 		} else {
 			try {
-				final Version v = new Version("" + l.get("version"));
-				final Version lv = langVersion.get(addon.plugin);
+				Version v = new Version("" + l.get("version"));
+				Version lv = langVersion.get(addon.plugin);
 				assert lv != null; // set in loadDefault()
 				if (v.isSmallerThan(lv))
 					Skript.warning(addon + "'s language file " + name + ".lang is outdated, some messages will be english.");
-			} catch (final IllegalArgumentException e) {
+			} catch (IllegalArgumentException e) {
 				Skript.error("Illegal version syntax in " + addon + "'s language file " + name + ".lang: " + e.getLocalizedMessage());
 			}
 		}
 		l.remove("version");
-		final HashMap<String, String> loc = localized;
+		HashMap<String, String> loc = localized;
 		if (loc != null)
 			loc.putAll(l);
 		else
@@ -287,50 +285,24 @@ public class Language {
 		return true;
 	}
 	
-	private static HashMap<String, String> load(final @Nullable InputStream in, final String name) {
+	private static HashMap<String, String> load(@Nullable InputStream in, String name) {
 		if (in == null)
 			return new HashMap<>();
 		try {
 			return new Config(in, name + ".lang", false, false, ":").toMap(".");
-		} catch (final IOException e) {
+		} catch (IOException e) {
 			Skript.exception(e, "Could not load the language file '" + name + ".lang': " + ExceptionUtils.toString(e));
 			return new HashMap<>();
 		} finally {
 			try {
 				in.close();
-			} catch (final IOException e) {}
+			} catch (IOException e) {}
 		}
 	}
-	
-	private static void validateLocalized() {
-		final HashMap<String, String> loc = localized;
-		if (loc == null) {
-			assert false;
-			return;
-		}
-		HashSet<String> s = new HashSet<>(english.keySet());
-		s.removeAll(loc.keySet());
-		removeIgnored(s);
-		if (!s.isEmpty() && Skript.logNormal())
-			Skript.warning("The following messages have not been translated to " + name + ": " + StringUtils.join(s, ", "));
-		s = new HashSet<>(loc.keySet());
-		s.removeAll(english.keySet());
-		removeIgnored(s);
-		if (!s.isEmpty() && Skript.logHigh())
-			Skript.warning("The localized language file(s) has/ve superfluous entries: " + StringUtils.join(s, ", "));
-	}
-	
-	private static void removeIgnored(final Set<String> keys) {
-		final Iterator<String> i = keys.iterator();
-		while (i.hasNext()) {
-			if (i.next().startsWith(Noun.GENDERS_SECTION))
-				i.remove();
-		}
-	}
-	
+
 	private final static List<LanguageChangeListener> listeners = new ArrayList<>();
 	
-	public static enum LanguageListenerPriority {
+	public enum LanguageListenerPriority {
 		EARLIEST, NORMAL, LATEST;
 	}
 	
@@ -345,11 +317,11 @@ public class Language {
 	 * 
 	 * @param l
 	 */
-	public static void addListener(final LanguageChangeListener l) {
+	public static void addListener(LanguageChangeListener l) {
 		addListener(l, LanguageListenerPriority.NORMAL);
 	}
 	
-	public static void addListener(final LanguageChangeListener l, final LanguageListenerPriority priority) {
+	public static void addListener(LanguageChangeListener l, LanguageListenerPriority priority) {
 		assert priority != null;
 		listeners.add(priorityStartIndices[priority.ordinal()], l);
 		for (int i = priority.ordinal() + 1; i < LanguageListenerPriority.values().length; i++)
@@ -379,16 +351,16 @@ public class Language {
 	 * @param b Whether to enable localisation or not
 	 * @return Previous state
 	 */
-	public static boolean setUseLocal(final boolean b) {
+	public static boolean setUseLocal(boolean b) {
 		if (useLocal == b)
 			return b;
 		if (localized == null)
 			return false;
 		useLocal = b;
-		for (final LanguageChangeListener l : listeners) {
+		for (LanguageChangeListener l : listeners) {
 			try {
 				l.onLanguageChange();
-			} catch (final Exception e) {
+			} catch (Exception e) {
 				Skript.exception(e, "Error while changing the language " + (b ? "from english to" : "to english from") + " " + name, "Listener: " + l);
 			}
 		}
