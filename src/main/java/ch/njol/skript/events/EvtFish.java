7801b8ee2039ec0873bd5dbceeb2c4b10a96d501
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-05-12 20:54:52 +0200

    Fixed an issue with fishing states

--- src/main/java/ch/njol/skript/events/EvtFish.java
+++ src/main/java/ch/njol/skript/events/EvtFish.java
@@ -18,22 +18,17 @@
  */
 package ch.njol.skript.events;
 
-import java.util.Arrays;
-import java.util.EnumSet;
-
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.SkriptParser;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.eclipse.jdt.annotation.Nullable;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.doc.Description;
-import ch.njol.skript.doc.Examples;
-import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
-import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.Literal;
-import ch.njol.skript.lang.SkriptEvent;
-import ch.njol.skript.lang.SkriptParser;
+import java.util.Arrays;
+import java.util.EnumSet;
 
 @Name("Fishing")
 @Description("Called when a player triggers a fishing event (catching a fish, failing, etc.)")
@@ -44,36 +39,35 @@ import ch.njol.skript.lang.SkriptParser;
 	"on fishing state of entity caught:",
 	"\tpush event-entity vector from entity to player"})
 public class EvtFish extends SkriptEvent {
-	
+
 	static {
 		Skript.registerEvent("Fishing", EvtFish.class, PlayerFishEvent.class, "[player] fish[ing] [state[s] [of] %-fishingstates%]");
 	}
-	
+
 	@SuppressWarnings("null")
 	EnumSet<PlayerFishEvent.State> states = EnumSet.noneOf(PlayerFishEvent.State.class);
-	
+
 	@Override
 	public boolean init(Literal<?>[] args, int matchedPattern, SkriptParser.ParseResult parseResult) {
 		Literal<PlayerFishEvent.State> states = (Literal<PlayerFishEvent.State>) args[0];
-		System.out.println(states);
 		if (states == null) {
 			return true;
 		} else {
-			this.states.addAll(Arrays.asList(states.getArray()));
+			this.states.addAll(Arrays.asList(states.getAll()));
+			return true;
 		}
-		return false;
 	}
-	
+
 	@Override
 	public boolean check(Event e) {
 		return states.isEmpty() || states.contains(((PlayerFishEvent) e).getState());
 	}
-	
+
 	@Override
 	public String toString(@Nullable Event e, boolean debug) {
 		if (states.isEmpty())
 			return "fishing";
 		return "fishing states of " + states;
 	}
-	
+
 }
