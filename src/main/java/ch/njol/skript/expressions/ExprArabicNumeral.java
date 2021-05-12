467c6de6e3a3ed3ea23552c4a7fc3506bfe6ba0d
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-05-12 20:16:56 +0200

    Moved to new algorithm

--- src/main/java/ch/njol/skript/expressions/ExprArabicNumeral.java
+++ src/main/java/ch/njol/skript/expressions/ExprArabicNumeral.java
@@ -1,82 +1,79 @@
 /**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * This file is part of Skript.
+ * <p>
+ * Skript is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Skript is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
  * Copyright Peter Güttinger, SkriptLang team and contributors
  */
 package ch.njol.skript.expressions;
-
-import java.util.HashMap;
-
-import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.util.RomanNumeral;
+import org.eclipse.jdt.annotation.Nullable;
+
+import java.util.List;
+
 
 @Name("Arabic Numeral")
 @Description("The Arabic Numeral of a Roman Numeral. Even though the usual maximum roman number is 3999 we added 'H' and 'G' respectively for the 10000 and 5000 numbers, to increase the maximum number.")
 @Examples("set {_r} to arabic numeral of \"XVI\"")
 @Since("INSERT VERSION")
 public class ExprArabicNumeral extends SimplePropertyExpression<String, Number> {
-	
-	private final static HashMap<Character, Integer> map = new HashMap<Character, Integer>() {{
-		put('H', 10000);
-		put('G', 5000);
-		put('M', 1000);
-		put('D', 500);
-		put('C', 100);
-		put('L', 50);
-		put('X', 10);
-		put('V', 5);
-		put('I', 1);
-	}};
-	
+
+
 	static {
 		register(ExprArabicNumeral.class, Number.class, "arabic num(ber|eral)", "string");
 	}
-	
+
 	@Nullable
-	public static Number toArabic(String n) {
-		if (!n.matches("^H{0,3}(MH|MG|G?M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")) return null;
-		int previous = 0;
+	public static Number toArabic(String romanNumeral) {
 		int result = 0;
-		for (int i = 0; i < n.length(); i++) {
-			Integer num = map.get(n.charAt(i));
-			result += num - (num >= previous ? previous * 2 : 0);
-			previous = num;
+
+		List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+		int i = 0;
+
+		while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+			RomanNumeral symbol = romanNumerals.get(i);
+			if (romanNumeral.startsWith(symbol.name())) {
+				result += symbol.getValue();
+				romanNumeral = romanNumeral.substring(symbol.name().length());
+			} else {
+				i++;
+			}
 		}
 		return result;
 	}
-	
+
 	@Nullable
 	@Override
 	public Number convert(String s) {
 		return toArabic(s);
 	}
-	
+
 	@Override
 	public Class<? extends Number> getReturnType() {
 		return Number.class;
 	}
-	
+
 	@Override
 	protected String getPropertyName() {
 		return "arabic numeral";
 	}
-	
+
 }
--- src/main/java/ch/njol/skript/expressions/ExprRomanNumeral.java
+++ src/main/java/ch/njol/skript/expressions/ExprRomanNumeral.java
@@ -1,94 +1,81 @@
 /**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * This file is part of Skript.
+ * <p>
+ * Skript is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Skript is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
  * Copyright Peter Güttinger, SkriptLang team and contributors
  */
 package ch.njol.skript.expressions;
-
-import java.util.HashMap;
-
-import org.apache.commons.lang.StringUtils;
-import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.util.RomanNumeral;
+import org.eclipse.jdt.annotation.Nullable;
+
+import java.util.List;
 
 @Name("Roman Numeral")
 @Description("The Roman Numeral of an Arabic Numeral. Even though the usual maximum roman number is 3999 we added 'H' and 'G' respectively for the 10000 and 5000 numbers, to increase the maximum number.")
 @Examples("set {_r} to roman numeral of 49")
 @Since("INSERT VERSION")
 public class ExprRomanNumeral extends SimplePropertyExpression<Number, String> {
-	
-	private final static HashMap<Integer, String> map = new HashMap<Integer, String>() {{
-		put(10000, "H");
-		put(5000, "G");
-		put(1000, "M");
-		put(500, "D");
-		put(100, "C");
-		put(50, "L");
-		put(10, "X");
-		put(5, "V");
-		put(1, "I");
-	}};
-	
+
+
 	static {
 		register(ExprRomanNumeral.class, String.class, "roman num(ber|eral)", "number");
 	}
-	
+
 	@Nullable
 	public static String toRoman(int number) {
-		if (number >= 40000 || number < 1) return null;
-		int significantDigit = 1;
-		while (number >= significantDigit) significantDigit *= 10;
-		significantDigit /= 10;
-		
-		String result = "";
-		while (number > 0) {
-			int lastNum = Math.floorDiv(number, significantDigit);
-			if (lastNum <= 3)
-				result += StringUtils.repeat(map.get(significantDigit), lastNum);
-			else if (lastNum == 4)
-				result += (map.get(significantDigit) + map.get(significantDigit * 5));
-			else if (lastNum <= 8)
-				result += (map.get(significantDigit * 5) + StringUtils.repeat(map.get(significantDigit), lastNum - 5));
-			else if (lastNum == 9)
-				result += (map.get(significantDigit) + map.get(significantDigit * 10));
-			number = (int) Math.floor(number % significantDigit);
-			significantDigit /= 10;
+		if ((number <= 0) || (number > 40000)) {
+			throw new IllegalArgumentException(number + " is not in range (0,40000]");
 		}
-		return result;
+
+		List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+		int i = 0;
+		StringBuilder sb = new StringBuilder();
+
+		while ((number > 0) && (i < romanNumerals.size())) {
+			RomanNumeral currentSymbol = romanNumerals.get(i);
+			if (currentSymbol.getValue() <= number) {
+				sb.append(currentSymbol.name());
+				number -= currentSymbol.getValue();
+			} else {
+				i++;
+			}
+		}
+		return sb.toString();
 	}
-	
+
 	@Nullable
 	@Override
 	public String convert(Number number) {
 		return toRoman(number.intValue());
 	}
-	
+
 	@Override
 	public Class<? extends String> getReturnType() {
 		return String.class;
 	}
-	
+
 	@Override
 	protected String getPropertyName() {
 		return "roman numeral";
 	}
-	
+
 }
