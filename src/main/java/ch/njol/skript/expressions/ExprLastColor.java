0c9d75c9d5db147c0fecc5980aa8839aa34121fe
APickledWalrus <apickledwalrus@gmail.com>
2021-06-30 07:29:42 -0400

    Add the license header
    Also allows usage of "s" at the end

--- src/main/java/ch/njol/skript/expressions/ExprLastColor.java
+++ src/main/java/ch/njol/skript/expressions/ExprLastColor.java
@@ -1,3 +1,22 @@
+/**
+ * This file is part of Skript.
+ *
+ * Skript is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Skript is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.doc.Description;
@@ -10,12 +29,12 @@ import org.jetbrains.annotations.Nullable;
 
 @Name("Last Color")
 @Description("The colors used at the end of a string. The colors of the returned string will be formatted with their symbols.")
-@Examples("set {_color} to the last color of \"<red>hey<blue>yo\"")
+@Examples("set {_color} to the last colors of \"<red>hey<blue>yo\"")
 @Since("INSERT VERSION")
 public class ExprLastColor extends SimplePropertyExpression<String, String> {
 
 	static {
-		register(ExprLastColor.class, String.class, "last color", "strings");
+		register(ExprLastColor.class, String.class, "last color[s]", "strings");
 	}
 
 	@Nullable
@@ -31,7 +50,7 @@ public class ExprLastColor extends SimplePropertyExpression<String, String> {
 
 	@Override
 	protected String getPropertyName() {
-		return "last color";
+		return "last colors";
 	}
 
 }
